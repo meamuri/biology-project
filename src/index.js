afc6dd7d7694cfd73dece9d81e62e08c2597ad2c
meamruri <meammf@yandex.ru>
2019-11-12 08:50:58 +0300

    add eslint to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 
 
-module.exports = async (event, res) => {
-    const { request, session, version } = event;
+export async function handler(event, res) {
+    const { request, session, version } = event
 
     res.end(JSON.stringify({
         version,
         session,
         response: {
-            text: "boop",
+            text: 'boop',
             end_session: false,
         }
     }))
-};
+}
