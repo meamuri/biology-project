c0b7c3edd301988be6358018057ce02ca3f0b87b
meamruri <meammf@yandex.ru>
2019-11-13 08:36:34 +0300

    add test for default function. Change default export function with new return value

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
 export async function handler(event, res) {
     const { request, session, version } = event
 
-    res.end(JSON.stringify({
+    return {
         version,
         session,
         response: {
             text: 'boop',
             end_session: false,
         }
-    }))
+    }
 }
