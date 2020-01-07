13133ac06bf75aa86014f149e8df0b7320330d66
meamruri <meammf@yandex.ru>
2020-01-07 13:20:04 +0300

    add async processing

--- data/parser/index.js
+++ data/parser/index.js
@@ -5,14 +5,34 @@ const results = [];
 
 const headers = ['Phylum', 'PhylumName', 'Family', 'FamilyName', 'Species', 'SpeciesName'];
 
-fs.createReadStream('../Classification.csv')
-    .pipe(csv({
-        headers,
-        mapValues: ({ header, index, value }) => value.trim()
-    }))
-    .on('data', data => {
-        results.push(data)
-    })
-    .on('end', () => {
-        console.log(results);
-    });
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+
+async function main() {
+    const conn = await MongoClient.connect(url);
+    const db = conn.db("flora");
+
+    const phylum = db.collection('phylum');
+    const family = db.collection('family');
+    const species = db.collection('species');
+
+    fs.createReadStream('../Classification.csv')
+        .pipe(csv({
+            headers,
+            mapValues: ({ header, index, value }) => value.trim()
+        }))
+        .on('data', async data => {
+            let res = await phylum.insertOne({
+                name: data.Phylum
+            });
+            results.push(data)
+        })
+        .on('end', () => {
+            console.log(results);
+            console.log('woof')
+        });
+}
+
+(async () => {
+    await main();
+})();
