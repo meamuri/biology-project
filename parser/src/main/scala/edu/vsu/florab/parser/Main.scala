a8a9b53cd6ee0555e323426fe89a45e94ec2acc7
meamruri <meammf@yandex.ru>
2020-01-08 00:20:08 +0300

    add cache for scala processing

--- parser/src/main/scala/edu/vsu/florab/parser/Main.scala
+++ parser/src/main/scala/edu/vsu/florab/parser/Main.scala
@@ -4,7 +4,21 @@ import java.io.File
 
 import com.github.tototoshi.csv.CSVReader
 
+import scala.collection.mutable
+
 object Main extends App {
   val reader = CSVReader.open(new File("data/Classification.csv"))
-  reader.all().foreach(println)
+  val cache: mutable.Map[Taxon, mutable.Map[Taxon, List[Taxon]]] = mutable.Map()
+  val taxonList = reader.all().map(e => {
+    val phylum = Taxon(e.head, e(1))
+    val family = Taxon(e(2), e(3))
+    val species = Taxon(e(4), e(5))
+    Row(phylum, family, species)
+  })
+  taxonList.foreach(e => {
+    val families = cache.getOrElseUpdate(e.phylum, mutable.Map())
+    val species = families.getOrElseUpdate(e.family, List())
+    families(e.family) = e.species :: species
+  })
+  println(cache)
 }
--- parser/src/main/scala/edu/vsu/florab/parser/package.scala
+++ parser/src/main/scala/edu/vsu/florab/parser/package.scala
@@ -2,7 +2,5 @@ package edu.vsu.florab
 
 package object parser {
   case class Taxon(name: String, rusLocaleName: String)
-  case class Phylum(name: String, rusLocaleName: String)
-  case class Family(name: String, rusLocaleName: String)
-  case class Species(name: String, rusLocaleName: String)
+  case class Row(phylum: Taxon, family: Taxon, species: Taxon)
 }
