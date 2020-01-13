814c1ba2bb28a0cebb80deadea7995927ef6949e
meamruri <meammf@yandex.ru>
2020-01-13 09:46:11 +0300

    add routing and ts component with type

--- floreact/src/App.tsx
+++ floreact/src/App.tsx
@@ -1,27 +1,55 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import logo from './logo.svg';
 import Config from './config';
 import './App.css';
 
+
+export interface MainProps {
+  title?: string,
+}
+
+const Index: React.FC<MainProps> = (props) => {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {Config.message}
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+  )
+}
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {Config.message}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+        <Switch>
+          <Route path="/flora">
+            {/*<Table/>*/}
+          </Route>
+          <Route path="/genus">
+            {/*<Genus name="meeh" species={species}/>*/}
+          </Route>
+          <Route path="/">
+            <Index title={Config.message} />
+          </Route>
+        </Switch>
+      </Router>
+  )
 }
 
 export default App;
