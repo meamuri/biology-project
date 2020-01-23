f3d838996582eec3539731b1262adc0cf5cf453b
meamruri <meammf@yandex.ru>
2020-01-23 22:48:56 +0300

    add mvn project to git

---
+++ florest/src/test/kotlin/edu/vsu/flora/florest/florest/FlorestApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.vsu.flora.florest.florest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlorestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
