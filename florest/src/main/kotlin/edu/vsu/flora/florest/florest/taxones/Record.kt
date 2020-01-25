e74412db3a37b21a6ce5c61381e19d9a960263a5
meamruri <meammf@yandex.ru>
2020-01-25 16:27:20 +0300

    change SubRecord data class: add id annotation

--- florest/src/main/kotlin/edu/vsu/flora/florest/florest/taxones/Record.kt
+++ florest/src/main/kotlin/edu/vsu/flora/florest/florest/taxones/Record.kt
@@ -12,4 +12,9 @@ data class Record(
         var ruLocaleName: String?
 )
 
-data class SubRecord(var id: String?, var name: String?, var ruLocaleName: String? = null)
+data class SubRecord(
+        // https://docs.spring.io/spring-data/data-mongo/docs/1.5.1.RELEASE/reference/html/mapping-chapter.html
+        @Id var id: String?,
+        var name: String?,
+        var ruLocaleName: String? = null
+)
