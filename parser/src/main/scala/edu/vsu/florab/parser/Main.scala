c2fd11a569e4bfac4344ca9c9fc97a5079fc897f
meamruri <meammf@yandex.ru>
2020-01-25 16:49:57 +0300

    add new datasets

--- parser/src/main/scala/edu/vsu/florab/parser/Main.scala
+++ parser/src/main/scala/edu/vsu/florab/parser/Main.scala
@@ -15,9 +15,10 @@ object Main extends App {
   val reader = CSVReader.open(new File("data/Classification.csv"))
   val cache: mutable.Map[Taxon, mutable.Map[Taxon, List[Taxon]]] = mutable.Map()
   val taxonList = reader.all().map(e => {
-    val phylum = Taxon(e.head, e(1))
-    val family = Taxon(e(2), e(3))
-    val species = Taxon(e(4), e(5))
+    val row = e.map(_.trim)
+    val phylum = Taxon(row.head, row(1))
+    val family = Taxon(row(2), row(3))
+    val species = Taxon(row(4), row(5))
     Row(phylum, family, species)
   })
   taxonList.foreach(e => {
