f672f2236095f770dbc6736d10d68908378afc2c
meamruri <meammf@yandex.ru>
2020-02-20 22:33:32 +0300

    impl jwt token filter

--- florest/src/main/kotlin/edu/vsu/flora/florest/florest/security/JwtAuthenticationFilter.kt
+++ florest/src/main/kotlin/edu/vsu/flora/florest/florest/security/JwtAuthenticationFilter.kt
@@ -1,5 +1,9 @@
 package edu.vsu.flora.florest.florest.security
 
+import edu.vsu.flora.florest.florest.user.PrincipalDetailsService
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Service
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
@@ -8,13 +12,40 @@ import javax.servlet.http.HttpServletResponse
 
 @Service
 class JwtAuthenticationFilter(
-
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val principalDetailsService: PrincipalDetailsService
 ) : OncePerRequestFilter() {
+
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        try {
+            val token = extractJwtFromHeader(request)
+            if (token != null && jwtTokenProvider.isValidToken(token)) {
+                val username = jwtTokenProvider.extractUsername(token)
+                val userDetails = principalDetailsService.loadUserByUsername(username)
+                val authentication = UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.authorities).apply {
+                        details = WebAuthenticationDetailsSource().buildDetails(request)
+                    }
+                SecurityContextHolder.getContext().authentication = authentication
+            }
+        } catch (e: Exception) {
+            // some logging
+        }
+
+        filterChain.doFilter(request, response)
     }
+
+    fun extractJwtFromHeader(request: HttpServletRequest): String? {
+        val authenticationHeader = request.getHeader("Authorization")
+        return authenticationHeader
+            ?.takeIf { it.isNotBlank() && it.startsWith("Bearer ") }
+            ?.let { it.substring(7) }
+    }
+
 }
