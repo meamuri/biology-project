a22fee29f2a01ec5f320bf0ac3152e6b005785c3
meamruri <meammf@yandex.ru>
2020-02-22 10:46:26 +0300

    fix mongo api method

--- florest/src/main/kotlin/edu/vsu/flora/florest/florest/taxones/TaxonController.kt
+++ florest/src/main/kotlin/edu/vsu/flora/florest/florest/taxones/TaxonController.kt
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 
@@ -31,7 +30,6 @@ class TaxonController(private val taxonService: TaxonService) {
     fun get(): Collection<Taxon.Phylum> = taxonService.format()
 
     @PostMapping("/species/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun updateSpecies(@PathVariable("id") id: String, @RequestBody dto: UpdateDTO): Taxon.Species =
         try {
             taxonService.update(id, dto)
