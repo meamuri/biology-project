87e7f61ca73e2c680cc7f2fae89b49ec31566c9e
meamruri <meammf@yandex.ru>
2020-02-22 23:22:38 +0300

    add basic login form

--- floreact/src/components/classification.tsx
+++ floreact/src/components/classification.tsx
@@ -17,32 +17,43 @@ export default class Classification extends React.Component<any, any> {
 
     render(): React.ReactElement {
         return (
-            <>
+            <div className="container">
                 <Navbar className="navbar justify-content-between navbar-expand-lg navbar-light bg-light">
                     <Navbar.Brand href="/">Flora</Navbar.Brand>
                     <Form inline>
-                        <Button onClick={this.handleShow}>Submit</Button>
+                        <Button onClick={this.handleShow}>Вход</Button>
                     </Form>
                 </Navbar>
-                <div className="container">
-                    <FloraComponent/>
-                </div>
+
+                <FloraComponent/>
 
                 <Modal show={this.state.show} onHide={this.handleClose}>
                     <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
+                        <Modal.Title>Введите пароль</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Логин</Form.Label>
+                                <Form.Control type="email" placeholder="логин" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Пароль</Form.Label>
+                                <Form.Control type="password" placeholder="пароль" />
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={this.handleClose}>
-                            Close
+                            Отмена
                         </Button>
                         <Button variant="primary" onClick={this.handleClose}>
-                            Save Changes
+                            Войти
                         </Button>
                     </Modal.Footer>
                 </Modal>
-            </>
+            </div>
         )
     }
 
