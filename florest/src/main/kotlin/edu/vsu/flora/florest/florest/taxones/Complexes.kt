5749bd347fca4529072ddacff9687140ef04a376
meamruri <meammf@yandex.ru>
2020-06-13 20:24:40 +0300

    complexes.kt unknown enum value bugfix

--- florest/src/main/kotlin/edu/vsu/flora/florest/florest/taxones/Complexes.kt
+++ florest/src/main/kotlin/edu/vsu/flora/florest/florest/taxones/Complexes.kt
@@ -5,4 +5,5 @@ enum class Complexes {
     CALCIPHILES,    // кальцефильный (A calcicole, calciphyte or calciphile)
     PSAMOPHILES,    // псаммофил
     HALOPHILES,     // галофильный
+    UNKNOWN,        // неизвестный
 }
