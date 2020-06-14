88e1623dc4e0f064f6718c99d02624901aa9ab4d
meamruri <meammf@yandex.ru>
2020-06-14 09:35:03 +0300

    provide changes

--- migrator/src/main/kotlin/edu/vsu/flora/migrator/scripts/M005TyposFixes.kt
+++ migrator/src/main/kotlin/edu/vsu/flora/migrator/scripts/M005TyposFixes.kt
@@ -19,28 +19,38 @@ class M005TyposFixes(database: CoroutineDatabase) : Migration(
     companion object {
         data class Species(val name: String)
         val renameDianthusPseudoarmeria = MigrationDescription("Dianthus pseudoarmeria без \"о\"") { db ->
-            val filter = Species::name eq ""
-            val update = Species::name setTo ""
+            val filter = Species::name eq "Dianthus pseudoarmeria Bieb."
+            val update = Species::name setTo "Dianthus pseudarmeria Bieb."
             db.getCollection<Species>().updateMany(filter, update)
         }
-        val renameConvolvolusToConvolvUlus = MigrationDescription("Dianthus ConvolvolusToConvolvUlus") { db ->
-
+        val renameConvolvolusToConvolvUlus = MigrationDescription("Convolvolus to ConvolvUlus") { db ->
+            val filter = Species::name eq "Convolvolus lineatus L."
+            val update = Species::name setTo "Convolvulus lineatus L."
+            db.getCollection<Species>().updateMany(filter, update)
         }
 
         val renameAsragalusTesticulatusToAsTragalus = MigrationDescription("rename AsragalusTesticulatus to AsTragalus") { db ->
-
+            val filter = Species::name eq "Asragalus testiculatus Pall."
+            val update = Species::name setTo "Astragalus testiculatus Pall."
+            db.getCollection<Species>().updateMany(filter, update)
         }
 
         val renameLimoniumPlathyphyllumToPlatyphyllum = MigrationDescription("rename Limonium plathyphyllum -> platyphyllum") { db ->
-
+            val filter = Species::name eq "Limonium plathyphyllum Lincz."
+            val update = Species::name setTo "Limonium platyphyllum Lincz."
+            db.getCollection<Species>().updateMany(filter, update)
         }
 
         val renamePolemoniumCoeruleumCaeruleum = MigrationDescription("rename Polemonium coeruleum -> cAeruleum") { db ->
-
+            val filter = Species::name eq "Polemonium coeruleum L."
+            val update = Species::name setTo "Polemonium caeruleum L."
+            db.getCollection<Species>().updateMany(filter, update)
         }
 
         val renameCoeloglossum = MigrationDescription("Coeloglossum viride -> заменить C на англ раскладку") { db ->
-
+            val filter = Species::name eq "Сoeloglossum viride (L.) C. Hartm."
+            val update = Species::name setTo "Coeloglossum viride (L.) C. Hartm."
+            db.getCollection<Species>().updateMany(filter, update)
         }
     }
 }
