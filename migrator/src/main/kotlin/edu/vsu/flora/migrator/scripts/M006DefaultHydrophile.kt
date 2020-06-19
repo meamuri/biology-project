2b98c407f18f930fda656f11fd780929ab1115cf
meamruri <meammf@yandex.ru>
2020-06-19 23:28:59 +0300

    add migration for default hydrophile

---
+++ migrator/src/main/kotlin/edu/vsu/flora/migrator/scripts/M006DefaultHydrophile.kt
@@ -0,0 +1,20 @@
+package edu.vsu.flora.migrator.scripts
+
+import com.mongodb.client.model.Filters
+import edu.vsu.flora.migrator.process.Migration
+import edu.vsu.flora.migrator.schema.MigrationDescription
+import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.setTo
+
+class M006DefaultHydrophile(database: CoroutineDatabase) : Migration(
+    database, 4, listOf(
+        migrationDescription
+    )
+) {
+    companion object {
+        data class Species(val hydrophile: String)
+        val migrationDescription = MigrationDescription("provide default hydrophile to each record") { db ->
+            db.getCollection<Species>().updateMany(Filters.exists("_id"), Species::hydrophile setTo "UNDEFINED")
+        }
+    }
+}
