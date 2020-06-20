d1987315bc70c438b3cf98a8d336cbb2348f52ae
meamruri <meammf@yandex.ru>
2020-06-20 09:54:52 +0300

    provide editing

--- floreact/src/lib/schema/hydrophilie/helpers.ts
+++ floreact/src/lib/schema/hydrophilie/helpers.ts
@@ -1,9 +1,34 @@
-import { Hydrophile } from './core'
+import {Hydrophile} from './core'
 
 export function toLocaleName(hydrophile: string): string {
     switch (hydrophile) {
+        case Hydrophile.XEROPHYTE:
+            return "Ксерофиты"
+        case Hydrophile.SCLEROPHYTE:
+            return "Склерофиты"
+        case Hydrophile.MESOPHYTE:
+            return "Мезофиты"
+        case Hydrophile.HYGROPHYTE:
+            return "Гигрофиты"
+        case Hydrophile.HYDROPHYTE:
+            return "Гидрофиты"
         case Hydrophile.UNDEFINED:
         default:
             return "Неизвестный"
     }
+}
+
+export function toHydrophile(hydrophile: string): Hydrophile {
+    if (hydrophile === Hydrophile.XEROPHYTE) {
+        return Hydrophile.XEROPHYTE
+    } else if (hydrophile === Hydrophile.MESOPHYTE) {
+        return Hydrophile.MESOPHYTE
+    } else if (hydrophile === Hydrophile.SCLEROPHYTE) {
+        return Hydrophile.SCLEROPHYTE
+    } else if (hydrophile === Hydrophile.HYDROPHYTE) {
+        return Hydrophile.HYDROPHYTE
+    } else if (hydrophile === Hydrophile.HYGROPHYTE) {
+        return Hydrophile.HYGROPHYTE
+    }
+    return Hydrophile.UNDEFINED
 }
